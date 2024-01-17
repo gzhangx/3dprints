dab1a639f3a23a3fd3e3c1bee5e79c8037ccdaf9
Gang Zhang <gzhangx@hotmail.com>
2024-01-16 22:44:10 -0500

    test

--- myhome/blindArdControl/blindArdControl.ino
+++ myhome/blindArdControl/blindArdControl.ino
@@ -21,17 +21,22 @@ void setup() {
 }
 
 void loop() {
+  delay(5000);
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  
+              
+  
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     //Serial.println("at");
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    delay(20);                       // waits 15 ms for the servo to reach the position
   }
+  delay(5000);
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     //Serial.println("at");
-    delay(15);                       // waits 15 ms for the servo to reach the position
+    delay(20);                       // waits 15 ms for the servo to reach the position
   }
 }
