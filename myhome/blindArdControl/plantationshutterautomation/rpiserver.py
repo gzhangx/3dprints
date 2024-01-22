3f4a1349eb2c1ce16578617b48040b426af9c4c8
Gang Zhang <gzhangx@hotmail.com>
2024-01-21 23:09:05 -0500

    adj min

--- myhome/blindArdControl/plantationshutterautomation/rpiserver.py
+++ myhome/blindArdControl/plantationshutterautomation/rpiserver.py
@@ -8,12 +8,12 @@ from machine import Pin, Timer, PWM
 import utime
 
 MID=1500000
-MIN=1000000
+MIN=0500000
 MAX=2000000
 
 pwm = PWM(Pin(15))
 pwm.freq(50)
-pwm.duty_ns(MID)
+pwm.duty_ns(MIN)
 
 pwmPins = {
     "GP15":True,
