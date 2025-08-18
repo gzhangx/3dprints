9fa1e61118e716e5d12e152c8fd1c31e70932930
Gang Zhang <gzhangx@hotmail.com>
2025-08-17 23:35:14 -0400

    initial speed

--- arduino/test/remoteread/remoteread.ino
+++ arduino/test/remoteread/remoteread.ino
@@ -12,25 +12,66 @@ struct PinInputStatus {
 };
 
 PinInputStatus remotePins []= {
-  {12, 0, 0, 0, 0}
+  {2, 0, 0, 0, 0},   //hor, right 2000, left 1000, mind 1500
+  {3, 0, 0, 0, 0}    //verticle, up 2000, down 1000, mid 1500
 };
 
-int TOTAL_HISTORY = 10;
-PinInputStatus history[10];
 
-int TOTAL_REMOTE_PINS=1;
+const int TOTAL_REMOTE_PINS=2;
 
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
+#define LEFT_PWM_PIN 9
+#define LEFT_DIR_PIN 10
+#define RIGHT_PWM_PIN 7
+#define RIGHT_DIR_PIN 8
+
+
+volatile unsigned long risingEdgeTime2 = 0;
+volatile unsigned long fallingEdgeTime2 = 0;
+volatile unsigned long pulseWidth2 = 0;
+
+volatile unsigned long risingEdgeTime3 = 0;
+volatile unsigned long fallingEdgeTime3 = 0;
+volatile unsigned long pulseWidth3 = 0;
+
+void measurePulseWidth2() {
+  if (digitalRead(2) == HIGH) {
+    // Rising edge detected
+    risingEdgeTime2 = micros();
+  } else {
+    // Falling edge detected
+    fallingEdgeTime2 = micros();
+    pulseWidth2 = fallingEdgeTime2 - risingEdgeTime2;
+  }
+}
+
+void measurePulseWidth3() {
+  if (digitalRead(3) == HIGH) {
+    // Rising edge detected
+    risingEdgeTime3 = micros();
+  } else {
+    // Falling edge detected
+    fallingEdgeTime3 = micros();
+    pulseWidth3 = fallingEdgeTime3 - risingEdgeTime3;
+  }
+}
+void setup() {  
   Serial.begin(19200);  
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(12, INPUT);
-  for (int i = 0; i < TOTAL_HISTORY; i++) {
-    history[i].val = -1;
-    history[i].lastReadTime = 0;
-  }
+  pinMode(3, INPUT);
+  pinMode(2, INPUT);  
+
+  attachInterrupt(digitalPinToInterrupt(3), measurePulseWidth3, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(2), measurePulseWidth2, CHANGE);
+
+  pinMode(LEFT_PWM_PIN, OUTPUT);    //left wheel, mag
+  pinMode(LEFT_DIR_PIN, OUTPUT);   //left wheel, pos, 0 forward, 1 back
+  pinMode(RIGHT_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_DIR_PIN, OUTPUT);
+
+
+  digitalWrite(LEFT_DIR_PIN, 0);
+  digitalWrite(RIGHT_DIR_PIN, 0);
 }
 
 //return 0 means waiting
@@ -88,15 +129,102 @@ int roundToNearestHundred(int val) {
     
     return result;
 }
-int historyAt = 0;
-unsigned long maxloops = microsecondsToClockCycles(1000) / 16;
+
+
 // the loop routine runs over and over again forever:
+const unsigned long PERIOD_MS=1000; //10 or 1
+int generate_pwm_signal(int duty_cycle, unsigned long current_time_ms) {
+    // Ensure duty cycle is within valid range (0 to 100)
+    if (duty_cycle < 0) duty_cycle = 0;
+    if (duty_cycle > 100) duty_cycle = 100;
+    
+    // PWM period is 1ms (1000Hz frequency)
+    const unsigned long period_ms = PERIOD_MS;
+    
+    // Calculate the time within the current PWM cycle (0 to period_ms)
+    unsigned long cycle_time = current_time_ms % period_ms;
+    
+    // Calculate the high time based on duty cycle (in microseconds, since period is 1ms = 1000us)
+    unsigned long high_time_us = (duty_cycle * 1000) / 100;
+    
+    // Return 1 (high) if within high time, 0 (low) otherwise
+    return (cycle_time * 1000 < high_time_us) ? 1 : 0;
+}
+
+//from 1000->2000 to -100/100
+int scalePwm(int remotePwm) {
+  if (remotePwm < 1000) remotePwm = 1000;
+  else if (remotePwm > 2000) remotePwm = 2000;
+  remotePwm = (remotePwm - 1500) / 5;
+  return remotePwm;
+}
+//between -100 to 100
+int generate_pwm_full(int scaledPwm) {
+  
+  int remotePwmNoSign = scaledPwm;
+  if (scaledPwm < 0) remotePwmNoSign = -scaledPwm;
+  int on = generate_pwm_signal(remotePwmNoSign, millis());
+  if (scaledPwm < 0) return -on;
+  return on;
+}
+
+int OLD_LEFT_PWM=0;
+int OLD_LEFT_DIR=0;
+int OLD_RIGHT_PWM=0;
+int OLD_RIGHT_DIR=0;
+void control_motors(int x, int y) {
+    // x: -100 (left) to 100 (right)
+    // y: -100 (reverse) to 100 (forward)
+    
+    // Compute motor speeds
+    int left_speed = y + x;
+    int right_speed = y - x;
+    
+    // Constrain to -100 to 100
+    left_speed = constrain(left_speed, -100, 100);
+    right_speed = constrain(right_speed, -100, 100);
+    
+    // Left motor
+    int leftPWM = generate_pwm_full(left_speed);
+    int leftDIR=(left_speed >= 0) ? HIGH : LOW;
+    digitalWrite(LEFT_DIR_PIN, leftDIR);
+    digitalWrite(LEFT_PWM_PIN, leftPWM);
+    if (OLD_LEFT_PWM != leftPWM) {
+      OLD_LEFT_PWM = leftPWM;
+      Serial.println(String("LEFT PWM")+ leftPWM+" left speed "+left_speed+" right speed "+right_speed+" x="+x+" y="+y );;
+    }
+    if (OLD_LEFT_DIR != leftDIR){
+      OLD_LEFT_DIR = leftDIR;
+      Serial.println(String("LEFT DIR")+ leftDIR );
+    }
+          
+    // Right motor
+    int rightPWM = generate_pwm_full(right_speed);
+    int rightDIR = (right_speed >= 0) ? HIGH : LOW;
+    digitalWrite(RIGHT_DIR_PIN, rightDIR);
+    digitalWrite(RIGHT_PWM_PIN, rightPWM);
+    if (OLD_RIGHT_PWM != rightPWM) {
+      OLD_RIGHT_PWM = rightPWM;
+      Serial.println(String("RIGHT PWM")+ rightPWM );
+    }
+    if (OLD_RIGHT_DIR != rightDIR){
+      OLD_RIGHT_DIR = rightDIR;
+      Serial.println(String("RIGHT DIR")+ rightDIR );
+    }
+}
 
 int LED = HIGH;
 unsigned long ledCount = 0;
-int curVals[] = {15};
-int newVals[] = {15};
+int curVals[] = {100, 100};
+int newValsUnscalled[] = {100, 100};
+int curValsUnscalled[] = {100, 100};
+int newVals[] = {100, 100};
+int pulesWidths[] = {0,0};
 void loop() {
+  noInterrupts(); // Temporarily disable interrupts to safely access pulseWidth
+  pulesWidths[0] = pulseWidth2;
+  pulesWidths[1] = pulseWidth3;
+  interrupts(); // Re-enable interrupts
   // read the input on analog pin 0:
   ledCount++;
   if (ledCount > 10000) {
@@ -111,21 +239,20 @@ void loop() {
    PinInputStatus *ps = remotePins + i;
    byte ret = checkPinPwm(ps);
    if (ret == 1)  {
-      newVals[i] = roundToNearestHundred(ps->diff);
+      newVals[i] = scalePwm(roundToNearestHundred(ps->diff));
+      newVals[i] = scalePwm(roundToNearestHundred(pulesWidths[i]));      
+      //newValsUnscalled[i] = scalePwm(roundToNearestHundred(ps->diff));
    }
   }
-  
+
+    control_motors(newVals[0], newVals[1]);
   
     for (int i = 0; i < TOTAL_REMOTE_PINS;i++) {      
       if (newVals[i] != curVals[i]) {
-        Serial.println(String("at ")+i+ " " + curVals[i]+" new=" + newVals[i] + " " );
+        //Serial.println(String("at ")+i+ " " + curVals[i]+" new=" + newVals[i] + " curValsUnscalled="+ newValsUnscalled[i]+ " Unscalled="+ curValsUnscalled[i]);
+        curValsUnscalled[i] = newValsUnscalled[i];
         curVals[i] = newVals[i];
       }
-    }
-    historyAt = 0;
-  
-  
-  //int d13= pulseIn(12, HIGH);// wait for a second  
-  //dsp+= d13;
-  //Serial.println(dsp);
+    }    
+ 
 }
